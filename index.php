af87a27d59665dce195d29ae0327b08a48d11a8c
Ryan Tran <s3201690@student.rmit.edu.au>
2017-03-23 23:07:19 +1100

    Added validation for customer registration form --------------------------------------------------------------------- Customer must have content in all fields to continue with registration, if not, send the user back with any existing content and error message.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include('/assets/header.inc');
 <h1>Booking Website</h1>
 </div>
 <div class='instructionsIndexDiv'>
-<p>Customers - Login' to see booking status and to make new bookings.</p>
+<p>Customers - 'Login' to see booking status and to make new bookings.</p>
 <p>If you do not have an account, please 'Register' and then login.</p>
 <p>Owners - 'Login' to see more functionalities.</p>
 </div>
@@ -47,33 +47,41 @@ if(isset($_SESSION['loginError']) && !empty($_SESSION['loginError'])){
 <div id='registerForm'>
 <div class='formBorder'>
 <h1>Customer Register</h1>
-
 <form method="post" action="processRegister.php">
 <table class="centreTable">
 <tr>
 <th>Username: </th>
-<td><input type="text" name="username"/></td>
+<td><input type="text" name="username" <?php if(isset($_SESSION['returnData'])){print "value='{$_SESSION['returnData'][0]}'";} ?>/></td>
 </tr>
 <tr>
 <th>Password: </th>
-<td><input type="password" name="password"/></td>
+<td><input type="password" name="password" <?php if(isset($_SESSION['returnData'])){print "value='{$_SESSION['returnData'][1]}'";} ?>/></td>
 </tr>
 <tr>
 <th>Full name: </th>
-<td><input type="text" name="fullname"/></td>
+<td><input type="text" name="fullname" <?php if(isset($_SESSION['returnData'])){print "value='{$_SESSION['returnData'][2]}'";} ?>/></td>
 </tr>
 <tr>
 <th>Address: </th>
-<td><input type="text" name="address"/></td>
+<td><input type="text" name="address" <?php if(isset($_SESSION['returnData'])){print "value='{$_SESSION['returnData'][3]}'";} ?>/></td>
 </tr>
 <tr>
 <th>Phone Number: </th>
-<td><input type="text" name="phone"/></td>
+<td><input type="text" name="phone" <?php if(isset($_SESSION['returnData'])){print "value='{$_SESSION['returnData'][4]}'";} ?>/></td>
 </tr>
 </table>
 <table class="centreTable">
 <tr><td><input type="submit" value="Register"/></td></tr>
 <?php
+/*Check if return data from processRegister.php is set, unset after use.*/
+if(isset($_SESSION['returnData']) && !empty($_SESSION['returnData'])){
+	print("<tr><td class='errorMessage'>\n");
+	print("<p> {$_SESSION['registerError']} </p>\n");
+	print("</td></tr>\n");
+	unset($_SESSION['returnData']);
+	unset($_SESSION['registerError']);
+}
+/*Prints out error message if set*/
 if(isset($_SESSION['registerError']) && !empty($_SESSION['registerError'])){
 	print("<tr><td class='errorMessage'>\n");
 	print("<p> {$_SESSION['registerError']} </p>\n");
