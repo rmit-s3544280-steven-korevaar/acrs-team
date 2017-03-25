9835df2180beceab1f3a1319029c32b641f3db0c
Christine Huynh <s3438653@student.rmit.edu.au>
2017-03-25 17:08:51 +1100

    Modified customerPage and customerBooking to show a single customer's booking calender and summary

--- customerBooking.php
+++ customerBooking.php
@@ -6,9 +6,70 @@ include('/assets/header.inc');
 <?php
 include('assets/customerBannerAndNav.inc');
 ?>
-<div class='contentHereDiv'>
-<p>Some Content of Customer Booking</p>
-</div>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Calendar</title>
+    <p>Booking Calender</p>
+	<link rel='stylesheet' href='fullcalendar/fullcalendar.css' />
+	<script src='lib/jquery.min.js'></script>
+	<script src='lib/moment.min.js'></script>
+	<script src='fullcalendar/fullcalendar.js'></script>
+</head>
+
+<body>
+	<div id='calendar'></div>
+</body>
+
+<script>
+	$(document).ready(function() {		
+		$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+		defaultView: 'basicDay',
+		editable: true,
+			
+		events: [{
+			title: 'Meeting',
+			start: '2017-03-20T10:30:00',
+			end: '2017-03-20T12:30:00'
+		},
+					
+		<?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "SEPT_Assignment_Part_1";
+            //Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            //Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+			$query = "SELECT * FROM booking WHERE username = '".$_SESSION['username']."'";
+			$results = mysqli_query($conn,$query);
+
+			while($row = mysqli_fetch_array($results)) {							
+				print_r("{");
+				print_r("title: '".$row['username']."',");
+	            print_r("start: '".$row['startDateTime']."',");
+				print_r("end: '".$row['endDateTime']."'");
+				print_r("},");
+			}
+		?>
+		],
+		dayClick: function() {
+		alert('a day has been clicked!');
+		$('#calendar').fullCalendar('next');
+		}})});
+	</script>
+<a href="customerPage.php"><button>View Summary</button></a>
+
 <!--Body End-->
 <?php
 include('assets/footer.inc');
--- customerPage.php
+++ customerPage.php
@@ -7,9 +7,40 @@ include('/assets/header.inc');
 include('assets/customerBannerAndNav.inc');
 ?>
 <div class='contentHereDiv'>
-<p>Some Content of Customer Summary</p>
+<p>Your Booking Summaries</p>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "SEPT_Assignment_Part_1";
+    
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT startDateTime, endDateTime, otherDetails, username FROM booking WHERE username = '".$_SESSION['username']."'";
+    $result = $conn->query($sql);
+    $num = 1;
+
+    if ($result->num_rows > 0) {
+        echo "<table border = '1'><tr><th>No.</th><th>Start Time</th><th>End Time</th><th>Details</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$num."</td><td>".$row["startDateTime"]."</td><td>".$row["endDateTime"]."</td><td>".$row["otherDetails"]."</td></tr>";
+            $num += 1;
+        }
+        echo "</table>";
+        } else {
+            echo "No Bookings to Display.";
+        }
+    $conn->close();
+?>
+<a href="customerBooking.php"><button>View Calender</button></a>
 </div>
 <!--Body End-->
 <?php
 include('assets/footer.inc');
 ?>
+
