305377cb594f0ab85d179374df8a696267369c35
Ryan Tran <s3201690@student.rmit.edu.au>
2017-05-08 23:15:40 +1000

    Seperated data checking from logic

--- assets/processForms/processAddEmployee.php
+++ assets/processForms/processAddEmployee.php
@@ -19,28 +19,19 @@ include('../../datalogging/Logger.php');
 Logger::configure('../../config.xml');
 $logger = Logger::getLogger("main");
 
-
 /* Instantiate database */
 include('./databaseClass.inc');
 
-/*Check if input data exists and Initialise variables to call easier*/
-$checkForData = array('employeeName','jobTitle','employeeID');
-$checkFlag = true;
-foreach($checkForData as $data){
-	if(empty($_POST[$data])){
-		$checkFlag = false;
-	}
-}
 /*If all data exists, continue with the adding employee process*/
-if($checkFlag == true){
+if( checkInputData() == true ){
 	$employeeName = $_POST['employeeName'];
 	$jobTitle = $_POST['jobTitle'];
 	$employeeID = $_POST['employeeID'];
 	$businessID = $_SESSION['abn'];
 	
-	$result = $db->insert("insert into employee values('$employeeName','$jobTitle','$businessID','$employeeID');");
-
-	if($result != false){
+	// Search existing employeeID, employeeID must not be existing in system.
+	if( searchExistingEmployeeID($db,$employeeID) == false ){
+		$result = $db->insert("insert into employee values('$employeeName','$jobTitle','$businessID','$employeeID');");
 		//If successful add, send back to businessPageEmployeeAddEmployee.php with success message.
 		$_SESSION['returnSuccessAddEmployeeMessage'] = "Successfully added new Employee.";
 		$logger->info("Owner succes added a new employee");
@@ -48,7 +39,7 @@ if($checkFlag == true){
 	}
 	else{
 	    /* If unsuccessfull, send back to businessPageEmployeeAddEmployee.php with error message.
-		  * Employee is already in system.
+		  * EmployeeID is already in system.
 		  */
 		$_SESSION['returnErrorAddEmployeeMessage'] = "A employee of that 'Employee Number' is already in the system.";
 		$logger->error("Error occured while owner trying to add a new employee, employee number already exists");
@@ -61,5 +52,22 @@ else{
 	header("location: ./../../businessPageEmployeeAddEmployee.php");
 }
 
+/* Function used to check if input data was posted sent to this php via _POST */
+function checkInputData(){
+	$checkForData = array('employeeName','jobTitle','employeeID');
+	foreach($checkForData as $data){
+		if(empty($_POST[$data])){
+			return false;
+		}
+	}
+	return true;
+}
+function searchExistingEmployeeID($db,$employeeID){
+	$result = $db->select("SELECT * FROM employee where employeeID = '$employeeID';");
+	if( mysqli_fetch_array($result) == null ){
+		return false;
+	}
+	return true;
+}
 exit(0);
 ?>
